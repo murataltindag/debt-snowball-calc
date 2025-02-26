ae6bc66e2781f7ad211d69397a08e96350dcc499
Murat Altindag <murataltindag3@gmail.com>
2025-02-26 17:10:07 -0600

    Fixed the popup and extra payment, still has interest issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3037827307"
+    serviceWorkerVersion: "1540466870"
   }
 });
